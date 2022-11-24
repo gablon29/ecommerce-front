bc8ca3adc00d3f573cf8bd16113313d46693c226
adolfo85 <adolfodeboeck@gmail.com>
2022-11-24 20:31:10 -0300

    eliminación de contador

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,16 +6,7 @@ import Tarjetas from "../tarjetas/Tarjetas";
 import Tabla from "../tabla/Tabla";
 
 const Home = () => {
-  const [contador1, setContador1] = useState(0);
-  const [contador2, setContador2] = useState(0);
-
-  const handleAumentar = () => {
-    setContador1(contador1 + 1);
-  };
-
-  const handleDisminuir = () => {
-    setContador1(contador1 - 1);
-  };
+  
 
   useEffect(() => {
     console.log(
@@ -29,34 +20,13 @@ const Home = () => {
 
   useEffect(() => {
     console.log("se ejecuto el useEffect cuando se modifica nuestra variable");
-  }, [contador1, contador2]);
+  };
 
   return (
     <>
       <Navegacion />
 
-      <Button className="btn btn-warning" onClick={handleAumentar}>
-        Aumentar
-      </Button>
-      <Button className="btn btn-success" onClick={handleDisminuir}>
-        Disminuir
-      </Button>
-      <h1>Contador: {contador1}</h1>
-
-      <Button
-        className="btn btn-primary"
-        onClick={() => setContador2(contador2 + 1)}
-      >
-        Aumentar
-      </Button>
-      <Button
-        className="btn btn-danger"
-        onClick={() => setContador2(contador2 - 1)}
-      >
-        Disminuir
-      </Button>
-      <h1>Contador2: {contador2}</h1>
-
+      
       {/* <Carrusel /> */}
       <Tarjetas />
       {/* <Tabla /> */}
