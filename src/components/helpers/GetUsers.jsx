b7edaf6b307a277e7066b9ba3e7d2fb3917cd7e1
Alfredo A. Correa <105443143+acorrea15@users.noreply.github.com>
2023-01-18 00:18:53 -0300

    Delete src directory

--- src/components/helpers/GetUsers.jsx
+++
@@ -1,9 +0,0 @@
-const GetUsers = async () => {
-    const url = "https://jsonplaceholder.typicode.com/users/";
-    const res = await fetch(url);
-    const data = await res.json();
-  
-    return data;
-  };
-  
-  export default GetUsers;
