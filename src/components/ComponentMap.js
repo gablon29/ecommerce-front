32e17a508b589014e427cd55494b538849f2e99f
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-01-26 11:59:37 -0300

    includMap

---
+++ src/components/ComponentMap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {GoogleMaps, widhtScriptjs, widhtGopgleMap} from 'react-google-map'
+
+const ComponentMap = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ComponentMap
