f0f285526914250916679efb3093a35090b1c830
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-01-27 17:15:21 -0300

    includingstyle

--- src/components/ComponentMap.js
+++ src/components/ComponentMap.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react';
-import credentialkey from './credentialkey';
-
-const compTransition = ({ text }) => <div>{text}</div>
 
 const ComponentMap = () => {
   const defaultProps = {
@@ -15,15 +12,11 @@ const ComponentMap = () => {
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: "AIzaSyC0VOoWyX1UcMzKpDREi8qyUSkvKgPBQbM" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <compTransition
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
+        
       </GoogleMapReact>
     </div>
  )
